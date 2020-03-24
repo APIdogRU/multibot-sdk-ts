74064610602d1588248fd0c989a6d0ef53013c91
Vladislav Veluga <vladislav805@yandex.com>
2020-03-25 02:49:25 +0300

    Fix import

--- src/telegram/utils/reply.ts
+++ src/telegram/utils/reply.ts
@@ -1,6 +1,6 @@
 import { Message } from '../types/message';
 import { SendMessageOptions } from '../types/send';
-import TelegramBot from '../';
+import { TelegramBot } from '../';
 
 export const reply = async(bot: TelegramBot, message: Message, text: string, options: SendMessageOptions = {}) => bot.request('sendMessage', {
     chat_id: message.chat.id,
